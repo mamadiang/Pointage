fab6cc3cdaf045b676cd7f08c43f05df9a9d3b3f
diang <md.diang11@gmail.com>
2025-01-09 00:33:39 +0100

    Initialisation du projet et mis en place de l'architecture de developpement

---
+++ Config/Sequelize.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize')
+
+const config = require('./config.json')
+
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: 'mysql',
+    port: config.port
+})
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,5 @@
+const app = require('./index');
+
+app.listen(3000, () =>{
+    console.log('votre serveur est lancé sur http://127.0.0.1:3000')
+})
